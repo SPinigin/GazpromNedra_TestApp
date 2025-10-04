5f4c56a4ac94d9299d0f9aaf6b72aff5f109756c
Sergey Pinigin <pinigin@live.com>
2025-10-04 14:12:53 +0500

    add well repo

---
+++ app/repo/well.py
@@ -0,0 +1,28 @@
+from typing import List
+from sqlalchemy.orm import Session
+from app.models.well import Well
+from app.schemas.well import WellCreate, WellUpdate
+from base import BaseRepo
+
+class WellRepo(BaseRepo[Well, WellCreate, WellUpdate]):
+
+    def get_by_license(
+            self,
+            db: Session,
+            *,
+            license_id: int,
+            skip: int = 0,
+            limit: int = 100
+    ) -> List[Well]:
+        return(
+            db.query(Well)
+            .filter(Well.license_id == license_id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
+
+    def count_by_license(self, db: Session, *, license_id: int) -> int:
+        return db.query(Well).filter(Well.license_id == license_id).count()
+
+    # well_repo = WellRepo(Well)
