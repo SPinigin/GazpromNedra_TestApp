fb26740982af2469f45572895945c5aa3ee46f68
Sergey Pinigin <pinigin@live.com>
2025-10-05 18:57:29 +0500

    add logging

---
+++ app/core/logging.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+from config import settings
+
+
+def setup_logging():
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(getattr(logging, settings.LOG_LEVEL))
+    root_logger.addHandler(console_handler)
+
+    if settings.DEBUG:
+        logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
